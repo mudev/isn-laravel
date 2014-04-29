cfe64812932a219b75d2b9ae0c44ccaee7afaa76
mu <muflexnet@gmail.com>
2014-04-29 20:11:29 +0100

    Primary stage

---
+++ .config.rb
@@ -0,0 +1,8 @@
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = ""
+sass_dir = "sass"
+images_dir = "images"
+fonts_dir = "css/fonts"
